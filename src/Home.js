b225c571b16e0d096bd34ff79462b765d4e00360
Thabo Lebelo <thabolebelo@icloud.com>
2022-04-15 14:41:49 +0200

    App state

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,20 @@
+import { useState } from "react";
+
 const Home = () => {
+    const [blogs, setBlogs] = useState([
+        { title: "Software development", body: "The best programming...", author: "Thabo", id: 1 },
+        { title: "Basic accounting rules", body: "Reconciliation is...", author: "Thabang", id: 2 },
+        { title: "Soccer tournament", body: "Made it to the final...", author: "Nkosi", id: 3 }
+    ]);
+
     return (
         <div className="home">
-            <h2>Homepage</h2>
+            {blogs.map((blog) => (
+                <div className="blog-preview" key={blog.id}>
+                    <h2>{blog.title}</h2>
+                    <p>Written by {blog.author}</p>
+                </div>
+            ))}
         </div>
     );
 };
