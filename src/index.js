88d913270bf18f185a90946e1a2542dd82f347ad
Thabo Lebelo <thabolebelo@icloud.com>
2022-04-15 15:23:51 +0200

    Get started with  Apollo Client

--- src/index.js
+++ src/index.js
@@ -2,11 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: 'http://localhost:3001/',
+  cache: new InMemoryCache()
+});
 
 ReactDOM.render(
-  <React.StrictMode>
+  <ApolloProvider client={client}>
     <App />
-  </React.StrictMode>,
+  </ApolloProvider>,
   document.getElementById('root')
 );
 
