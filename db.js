f78bd1f20cbbcaa96f4a3d52828d4e2ca482a549
Thabo Lebelo <thabolebelo@icloud.com>
2022-04-15 14:41:33 +0200

    JSON graphql-server

---
+++ db.js
@@ -0,0 +1,12 @@
+module.exports = {
+    blogs: [
+        { title: "Software development", body: "The best programming...", author_id: 1, id: 1 },
+        { title: "Basic accounting rules", body: "Reconciliation is...", author_id: 2, id: 2 },
+        { title: "Soccer tournament", body: "Made it to the final...", author_id: 3, id: 3 }
+    ],
+    authors: [
+        { id: 1, name: "Thabo" },
+        { id: 2, name: "Thabang" },
+        { id: 3, name: "Nkosi" }
+    ]
+}
