8513c2a41c83a07e899f2b5cd48ad8c3949c8071
Thabo Lebelo <thabolebelo@icloud.com>
2022-04-16 11:27:45 +0200

    Fetch data using the GraphQL API

--- src/Home.js
+++ src/Home.js
@@ -1,19 +1,46 @@
-import { useState } from "react";
+// import { useState } from "react";
+import { useQuery, gql } from "@apollo/client";
+
+const BLOGS_QUERY = gql`
+    query {
+        allBlogs {
+            title
+            body
+            Author {
+                name
+            }
+        }
+    }
+`;
 
 const Home = () => {
-    const [blogs, setBlogs] = useState([
-        { title: "Software development", body: "The best programming...", author: "Thabo", id: 1 },
-        { title: "Basic accounting rules", body: "Reconciliation is...", author: "Thabang", id: 2 },
-        { title: "Soccer tournament", body: "Made it to the final...", author: "Nkosi", id: 3 }
-    ]);
+
+    const { loading, error, data } = useQuery(BLOGS_QUERY);
+
+    // const [blogs, setBlogs] = useState([
+    //     { title: "Software development", body: "The best programming...", author: "Thabo", id: 1 },
+    //     { title: "Basic accounting rules", body: "Reconciliation is...", author: "Thabang", id: 2 },
+    //     { title: "Soccer tournament", body: "Made it to the final...", author: "Nkosi", id: 3 }
+    // ]);
 
     return (
         <div className="home">
-            {blogs.map((blog) => (
+            {/* {blogs.map((blog) => (
                 <div className="blog-preview" key={blog.id}>
                     <h2>{blog.title}</h2>
                     <p>Written by {blog.author}</p>
                 </div>
+            ))} */}
+            {error && <p>Error :(</p>}
+            {loading && <p>Loading...</p>}
+            {data && Object.values(data).map(blogs => (
+                blogs.map((blog, index) => (
+                    
+                    <div className="blog-preview" key={index}>
+                        <h2>{blog.title}</h2>
+                        <p>Written by {blog.Author.name}</p>
+                    </div>
+                ))
             ))}
         </div>
     );
